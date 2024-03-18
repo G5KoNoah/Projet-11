21bad22e64aadcadfabd4c42506f466a8341861f
Lilian501 <llafin@gaming.tech>
2024-03-18 19:35:47 +0100

    input test fait

--- Projet C#/Input.cs
+++ Projet C#/Input.cs
@@ -4,9 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Projet_C_
 {
     internal class Input
     {
+       
+        public Input()
+        {
+            
+            
+        }
+        public void InputTest()
+        {
+            ConsoleKeyInfo  key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.LeftArrow)
+            {
+                Console.WriteLine("Flèche gauche pressée");
+            }
+            if (key.Key == ConsoleKey.RightArrow)
+            {
+                Console.WriteLine("Flèche droite pressée");
+            }
+            if (key.Key == ConsoleKey.DownArrow)
+            {
+                Console.WriteLine("Flèche du bas pressée");
+            }
+            if (key.Key == ConsoleKey.UpArrow)
+            {
+                Console.WriteLine("Flèche du haut pressée");
+            }
+        }
+
+        
     }
 }
--- Projet C#/Program.cs
+++ Projet C#/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Projet_C_;
+
+
 Console.WriteLine("Hello, World!");
+Input input = new Input();
+
+while (true)
+{
+    input.InputTest();  
+}
+
+
+ 
