1f1be6b0cd04c1e17d074c0bf42f4d3a694e149f
nleguyader <nleguyader@gaming.tech>
2024-03-19 12:50:11 +0100

    alide

--- Projet C#/Program.cs
+++ Projet C#/Program.cs
@@ -4,12 +4,8 @@ using Projet_C_;
 //Console.WriteLine("Hello, World!");
 Map test = new Map();
 Draw draw = new Draw();
-var map = test.fileToText("..\\..\\..\\map1.txt");
-draw.DrawMap(map);
-// See https://aka.ms/new-console-template for more information
-using Projet_C_;
-
-
+//var map = test.fileToText("..\\..\\..\\map1.txt");
+//draw.DrawMap(map);
 Console.WriteLine("Hello, World!");
 Input input = new Input();
 
